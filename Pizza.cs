ce99ec34cdbe4d804d8c5295b0fa33fe4053f491
aleixRodriguez-commits <aleixoo19@gmail.com>
2025-10-28 09:23:07 +0000

    3 commit

--- Pizza.cs
+++ Pizza.cs
@@ -7,6 +7,40 @@ namespace webapi
         public string Description { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
         public List<Ingrediente> Ingredientes { get; set; } = new();
+        public decimal Price => 
+            Ingredientes.Sum(i => i.Costo) * 1.2m;
+
+
+        public decimal GetPrecio()
+        {
+            var costoTot = Ingredientes.Sum(i => i.Costo);
+            return Math.Round(costoTotal * 1.2m, 2);
+        }
+
+        public void AddIngrediente(Ingrediente ingrediente)
+        {
+                Ingredientes.Add(ingrediente);
+        }
+
+        public bool EliminarIngrediente(int ingredienteId)
+        {
+                var ingrediente = Ingredientes.FirstOrDefault(i => i.Id == ingredienteId);
+                if (ingrediente == null) return false;
+
+                Ingredientes.Remove(ingrediente);
+                return true;
+        } 
+
+        public bool ActualizarCosteIngrediente(int ingredienteId, decimal nuevoCoste)
+        {
+            var ingrediente = Ingredientes.FirstOrDefault(i => i.Id == ingredienteId);
+            if (ingrediente == null) return false;
+
+            ingrediente.Costo = nuevoCosto;
+            return true;
+        }
+
+
 
     }
 }
