e77400adbb3b034889b5cfcfaa893a5829820aea
aleixRodriguez-commits <aleixoo19@gmail.com>
2025-10-28 08:42:29 +0000

    second commit

---
+++ Ingrediente.cs
@@ -0,0 +1,9 @@
+namespace webapi
+{
+    public class Ingrediente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Costo { get; set; }
+    }
+}
